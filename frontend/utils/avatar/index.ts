eeb0d551c5694c2ccfaab3f864ce1e687c7c3c98
nvsriram <nuranisri01@gmail.com>
2023-04-05 10:27:36 -0500

    Optimised avatar gen, fixed avatar overwrite bug

--- frontend/utils/avatar/index.ts
+++ frontend/utils/avatar/index.ts
@@ -2,9 +2,8 @@ import { Connection, Keypair, PublicKey, SystemProgram, Transaction, sendAndConf
 import BN from "bn.js";
 import { DATA_PROGRAM_ID, PDA_SEED, AVATAR_PROGRAM_ID } from "./constants";
 import { svgPKs } from "./svg-pubkeys";
-import { YubikeySigner, KeypairSigner, Signer } from "../../types/account";
+import { KeypairSigner, Signer } from "../../types/account";
 import { sendAndConfirmTransactionWithAccount } from "..";
-import nacl from "tweetnacl";
 
 export const generateAvatar = async (connection: Connection, wallet: Signer, identity: PublicKey, update: () => void) => {
     const feePayerPK = await wallet.getPublicKey();
@@ -18,35 +17,6 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
     let pdaData: PublicKey;
       const dataAccountKP = new Keypair();
       const dataAccountSigner = new KeypairSigner(dataAccountKP);
-      const rentExemptAmount = await connection.getMinimumBalanceForRentExemption(
-        200
-      );
-      const createIx = SystemProgram.createAccount({
-        fromPubkey: await wallet.getPublicKey(),
-        newAccountPubkey: dataAccountKP.publicKey,
-        lamports: rentExemptAmount,
-        space: 200,
-        programId: DATA_PROGRAM_ID,
-      });
-      recentBlockhash = await connection.getLatestBlockhash();
-      const createTx = new Transaction({
-      feePayer: feePayerPK,
-      ...recentBlockhash,
-    });
-      createTx.add(createIx);
-      const createTxid = await sendAndConfirmTransactionWithAccount(
-        connection,
-        createTx,
-        [wallet, dataAccountSigner],
-        {
-          skipPreflight: true,
-          preflightCommitment: "confirmed",
-          confirmation: "confirmed",
-        } as ConfirmOptions
-      );
-      console.log(
-        `create: https://explorer.solana.com/tx/${createTxid}?cluster=devnet`
-      );
 
       [pdaData] = PublicKey.findProgramAddressSync(
         [Buffer.from(PDA_SEED, "ascii"), dataAccountKP.publicKey.toBuffer()],
@@ -56,7 +26,7 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
       const space = new BN(200).toArrayLike(Buffer, "le", 8);
       const dynamic = Buffer.from(new Uint8Array([1]));
       const authority = feePayerPK.toBuffer();
-      const is_created = Buffer.from(new Uint8Array([1]));
+      const is_created = Buffer.from(new Uint8Array([0]));
       const false_flag = Buffer.from(new Uint8Array([0]));
       const initializeIx = new TransactionInstruction({
         keys: [
@@ -112,7 +82,8 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
       );
       update();
       dataAccount = dataAccountKP.publicKey;
-  
+        
+    const partsTx: Transaction[] = [];
     const initializeIdentityIx = new TransactionInstruction({
       keys: [
         {
@@ -154,27 +125,10 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
       isSigner: false,
       isWritable: false,
     });
-    recentBlockhash = await connection.getLatestBlockhash();
-    const initializeIdentityTx = new Transaction({
-      feePayer: feePayerPK,
-      ...recentBlockhash,
-    });
+    const initializeIdentityTx = new Transaction();
     initializeIdentityTx.add(initializeIdentityIx);
-    const initializeIdentityTxid = await sendAndConfirmTransactionWithAccount(
-      connection,
-      initializeIdentityTx,
-      [wallet],
-      {
-        skipPreflight: true,
-        preflightCommitment: "confirmed",
-        confirmation: "confirmed",
-      } as ConfirmOptions
-    );
-    console.log(
-      `start: https://explorer.solana.com/tx/${initializeIdentityTxid}?cluster=devnet`
-    );
-    update();
-  
+    partsTx.push(initializeIdentityTx);
+    
     const appendIdentityCloIx = new TransactionInstruction({
       keys: [
         {
@@ -213,27 +167,9 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
         isWritable: false,
       });
     });
-    recentBlockhash = await connection.getLatestBlockhash();
-    const appendIdentityCloTx = new Transaction({
-      feePayer: feePayerPK,
-      ...recentBlockhash,
-    });
+    const appendIdentityCloTx = new Transaction();
     appendIdentityCloTx.add(appendIdentityCloIx);
-    const appendIdentityCloTxid = await sendAndConfirmTransactionWithAccount(
-      connection,
-      appendIdentityCloTx,
-      [wallet],
-      {
-        skipPreflight: true,
-        preflightCommitment: "confirmed",
-        confirmation: "confirmed",
-      } as ConfirmOptions
-    );
-    console.log(
-      `clo: https://explorer.solana.com/tx/${appendIdentityCloTxid}?cluster=devnet`
-    );
-    update();
-
+    partsTx.push(appendIdentityCloTx);
   
     const appendIdentityTopIx = new TransactionInstruction({
       keys: [
@@ -273,26 +209,9 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
         isWritable: false,
       });
     });
-    recentBlockhash = await connection.getLatestBlockhash();
-    const appendIdentityTopTx = new Transaction({
-      feePayer: feePayerPK,
-      ...recentBlockhash,
-    });
+    const appendIdentityTopTx = new Transaction();
     appendIdentityTopTx.add(appendIdentityTopIx);
-    const appendIdentityTopTxid = await sendAndConfirmTransactionWithAccount(
-      connection,
-      appendIdentityTopTx,
-      [wallet],
-      {
-        skipPreflight: true,
-        preflightCommitment: "confirmed",
-        confirmation: "confirmed",
-      } as ConfirmOptions
-    );
-    console.log(
-      `top: https://explorer.solana.com/tx/${appendIdentityTopTxid}?cluster=devnet`
-    );
-    update();
+    partsTx.push(appendIdentityTopTx);
   
     const appendIdentityEyesIx = new TransactionInstruction({
       keys: [
@@ -332,26 +251,9 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
         isWritable: false,
       });
     });
-    recentBlockhash = await connection.getLatestBlockhash();
-    const appendIdentityEyesTx = new Transaction({
-      feePayer: feePayerPK,
-      ...recentBlockhash,
-    });
+    const appendIdentityEyesTx = new Transaction();
     appendIdentityEyesTx.add(appendIdentityEyesIx);
-    const appendIdentityEyesTxid = await sendAndConfirmTransactionWithAccount(
-      connection,
-      appendIdentityEyesTx,
-      [wallet],
-      {
-        skipPreflight: true,
-        preflightCommitment: "confirmed",
-        confirmation: "confirmed",
-      } as ConfirmOptions
-    );
-    console.log(
-      `eyes: https://explorer.solana.com/tx/${appendIdentityEyesTxid}?cluster=devnet`
-    );
-    update();
+    partsTx.push(appendIdentityEyesTx);
   
     const appendIdentityMouthIx = new TransactionInstruction({
       keys: [
@@ -391,26 +293,9 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
         isWritable: false,
       });
     });
-    recentBlockhash = await connection.getLatestBlockhash();
-    const appendIdentityMouthTx = new Transaction({
-      feePayer: feePayerPK,
-      ...recentBlockhash,
-    });
+    const appendIdentityMouthTx = new Transaction();
     appendIdentityMouthTx.add(appendIdentityMouthIx);
-    const appendIdentityMouthTxid = await sendAndConfirmTransactionWithAccount(
-      connection,
-      appendIdentityMouthTx,
-      [wallet],
-      {
-        skipPreflight: true,
-        preflightCommitment: "confirmed",
-        confirmation: "confirmed",
-      } as ConfirmOptions
-    );
-    console.log(
-      `mouth: https://explorer.solana.com/tx/${appendIdentityMouthTxid}?cluster=devnet`
-    );
-    update();
+    partsTx.push(appendIdentityMouthTx);
   
     const completeIdentityIx = new TransactionInstruction({
       keys: [
@@ -443,26 +328,28 @@ export const generateAvatar = async (connection: Connection, wallet: Signer, ide
       programId: AVATAR_PROGRAM_ID,
       data: Buffer.concat([Buffer.from(new Uint8Array([5]))]),
     });
-    recentBlockhash = await connection.getLatestBlockhash();
-    const completeIdentityTx = new Transaction({
-      feePayer: feePayerPK,
-      ...recentBlockhash,
-    });
+    const completeIdentityTx = new Transaction();
     completeIdentityTx.add(completeIdentityIx);
-    const completeIdentityTxid = await sendAndConfirmTransactionWithAccount(
-      connection,
-      completeIdentityTx,
-      [wallet],
-      {
-        skipPreflight: true,
-        preflightCommitment: "confirmed",
-        confirmation: "confirmed",
-      } as ConfirmOptions
-    );
-    console.log(
-      `complete: https://explorer.solana.com/tx/${completeIdentityTxid}?cluster=devnet`
-    );
-    update();
+    partsTx.push(completeIdentityTx);
+
+    recentBlockhash = await connection.getLatestBlockhash();
+    for (const tx of partsTx) {
+      tx.feePayer = feePayerPK;
+      tx.recentBlockhash = recentBlockhash.blockhash;
+      const txid = await sendAndConfirmTransactionWithAccount(
+        connection,
+        tx,
+        [wallet],
+        {
+          skipPreflight: true,
+          preflightCommitment: "confirmed",
+          confirmation: "confirmed",
+        } as ConfirmOptions
+      );
+      console.log(`https://explorer.solana.com/tx/${txid}?cluster=devnet`);
+      update();
+    }
+
     console.log("Data Account: ", dataAccount.toString());
     return dataAccount;
 }
