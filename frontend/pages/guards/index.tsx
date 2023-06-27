2b4d3615bb44afeeb6d831f5b02894d81f068a93
nil <nilz3ro@protonmail.com>
2023-06-26 22:02:24 -0400

    create native sol transfer guard.

--- frontend/pages/guards/index.tsx
+++ frontend/pages/guards/index.tsx
@@ -1,36 +1,84 @@
+import { Button } from "antd";
+import { PublicKey, Transaction } from "@solana/web3.js";
 import { NextPage } from "next";
 import { Input } from "antd";
 import { useState, useEffect } from "react";
 import * as krypton from "../../js/src/generated";
-import { getCurrentAccount, getGuardPDA } from "../../utils";
+import { getCurrentAccount, getGuardPDA, sendAndConfirmTransactionWithAccount } from "../../utils";
 import { useGlobalModalContext } from "../../components/GlobalModal";
-import { KryptonAccount } from "../../types/account";
+import { KryptonAccount, YubikeyAccount } from "../../types/account";
+import { Connection } from "@solana/web3.js";
+import { RPC_URL } from "../../utils/constants";
+import { useGlobalState } from "../../context";
 
 
 const Guards: NextPage = () => {
     // TODO: lookup all guards here
     const [currentAccount, setCurrentAccount] = useState<KryptonAccount | undefined>();
+    const [guardAddress, setGuardAddress] = useState<PublicKey | undefined>();
     const ctx = useGlobalModalContext();
-    getCurrentAccount(ctx).then(setCurrentAccount);
+    const { network } = useGlobalState();
+    const connection = new Connection(RPC_URL(network), "confirmed");
 
     useEffect(() => {
         const setup = async () => {
-            console.log("In guard setup effect");
-            const guardAddress = getGuardPDA(await currentAccount!.getPublicKey())
-            console.log("guard address? :", guardAddress.toString());
+            const currentAccount = await getCurrentAccount(ctx);
+            setCurrentAccount(currentAccount);
+
+            const [guardAddress] = getGuardPDA(new PublicKey(currentAccount!.pda))
+            const maybeGuardAccount = await connection.getAccountInfo(guardAddress);
+
+
+            if (maybeGuardAccount) {
+                let guard = krypton.GuardAccount.fromAccountInfo(maybeGuardAccount);
+                console.log("found guard", guard);
+            }
         };
         setup().then(() => {
-            console.log("guards setup complete")
+            console.log("guards setup complete");
         });
-    })
+    }, []);
 
     return (
         <div>
             <h1 className={"title"}>Guards</h1>
             <p>Native SOL transfer guard</p>
-            {/* native sol transfer guard component */}
+            {guardAddress && (<p>Guard account: {guardAddress.toBase58()}</p>)}
+
+            <Button type="primary" onClick={() => {
+                if (!currentAccount) {
+                    console.error("no current account");
+                    return;
+                }
+                createNativeSolTransferGuard(currentAccount, connection, (guardAddress) => setGuardAddress(guardAddress))
+            }}>Create Guard</Button>
         </div>
     );
+};
+
+const createNativeSolTransferGuard = async (currentAccount: KryptonAccount | YubikeyAccount, connection: Connection, callback: (guardAddress: PublicKey) => void) => {
+    const [guardAddress] = getGuardPDA(new PublicKey(currentAccount.pda));
+    const createNativeSolTransferGuardInstruction = krypton.createInitializeNativeSolTransferGuardInstruction({
+        profileInfo: new PublicKey(currentAccount.pda),
+        authorityInfo: await currentAccount.getPublicKey(),
+        guardInfo: guardAddress
+    }, {
+        initializeNativeSolTransferGuardArgs: {
+            target: new PublicKey(currentAccount.pda),
+            transferAmount: 1000
+        }
+    });
+
+    let {blockhash} = await connection.getLatestBlockhash();
+
+    let transaction = new Transaction();
+    transaction.recentBlockhash = blockhash;
+    transaction.feePayer = await currentAccount.getPublicKey();
+    transaction.add(createNativeSolTransferGuardInstruction);
+
+    let sig = await sendAndConfirmTransactionWithAccount(connection, transaction, [currentAccount]);
+
+    callback(guardAddress);
 };
 
 export default Guards;
